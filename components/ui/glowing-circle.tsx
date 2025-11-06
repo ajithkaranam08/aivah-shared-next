b1e21318789f78fc2f3d58bab5f21a461cdd31cb
shahin s. hussain <shahinhuzzain@gmail.com>
2025-11-06 17:38:10 +0530

    audio connect

--- components/ui/glowing-circle.tsx
+++ components/ui/glowing-circle.tsx
@@ -1,13 +1,13 @@
-const GlowingCircle = () => {
+const GlowingLinear = () => {
 
     return (
         <div
             className="w-64 h-64 rounded-full 
-        bg-linear-to-tr from-pink-400 via-white to-cyan-400
-        animate-glow shadow-[0_0_50px_20px_rgba(255,255,255,0.1)]"
+        bg-linear-to-tr from-pink-400 via-white to-cyan-400 bg-size-[250%_250%]
+        animate-glow shadow-[0_0_50px_20px_rgba(255,255,255,0.1)] transition-colors outline outline-black/25 "
         ></div>
 
     );
 };
 
-export default GlowingCircle;
+export default GlowingLinear;
