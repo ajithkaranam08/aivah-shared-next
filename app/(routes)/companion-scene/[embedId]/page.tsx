b1b3fb5ad93c6c6b55905845ccbfa6c911de1d55
shahin s. hussain <shahinhuzzain@gmail.com>
2025-11-10 13:26:07 +0530

    navigate issue

--- app/(routes)/companion-scene/[embedId]/page.tsx
+++ app/(routes)/companion-scene/[embedId]/page.tsx
@@ -5,13 +5,25 @@ import { useParams } from "next/navigation";
 import { useValidateUUID } from "@/services/validate/query";
 
 import ChatCompanionScene from "../_components/chat-companion-scene";
+import { useEffect } from "react";
+import { SESSION_TOKEN } from "@/helper/storage";
 
 const CompanionEmbedId = () => {
   const { embedId } = useParams();
 
-  const { data } = useValidateUUID(String(embedId), {
+  const { data, isSuccess } = useValidateUUID(String(embedId), {
     enableScene: 1,
   });
+
+
+
+  useEffect(() => {
+    const token = SESSION_TOKEN.get();
+    if (!token && isSuccess) {
+      SESSION_TOKEN.set(data.details.token);
+    }
+  }, [isSuccess, data]);
+
 
   return (
     <div className="h-full">
